454bef7f220eae65e4fac81a361ef7d9d56e6f7b
jdofat <143310236+jdofat@users.noreply.github.com>
2025-05-11 15:40:42 -0400

    Update BookingConfirmationPage.js

--- BookingConfirmationPage.js
+++ BookingConfirmationPage.js
@@ -1,3 +1,5 @@
+/*
 Booking Confirmation Page:
-1. After clicking "Checkout," redirect to a thank you page and save the booking to Firebase
-
+1. After clicking "Checkout," redirect to a thank you page
+2. Save the booking to Firebase
+*/
